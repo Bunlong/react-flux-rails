35c4b846d20b78392e2cea4a3b9e2ad169062eb1
Bunlong <bunlong.van@gmail.com>
2015-11-19 10:54:16 +0700

    init project and add version of gem

---
+++ lib/react/flux/rails.rb
@@ -0,0 +1,9 @@
+require "react/flux/rails/version"
+
+module React
+  module Flux
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/react/flux/rails/version.rb
@@ -0,0 +1,7 @@
+module React
+  module Flux
+    module Rails
+      VERSION = "2.6.4"
+    end
+  end
+end
